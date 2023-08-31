6a38fcbdb9d6be278a48b900ec566a5bbff048f8
Elicarlos <elicarlosantos_@hotmail.com>
2023-08-31 17:14:28 -0300

    :bug: fix: Feito correção de rotas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Empresa(models.Model):
+    cnpj = models.CharField(max_length=20)
+    razao_social = models.CharField(max_length=100)
+    fantasia = models.CharField(max_length=100)
